a0a783d316be101514fd22d78908113f9b28dbfe
speedbomb <typo3@speedbomb.de>
2016-05-13 15:29:44 +0200

    Parser class - work in progress;

--- Classes/Service/ParserService.php
+++ Classes/Service/ParserService.php
@@ -43,27 +43,55 @@ class ParserService implements SingletonInterface {
 
     /**
      * The content object
+     *
      * @var ContentObjectRenderer
      */
     protected $cObj;
 
     /**
+     * Terms that should not be rendered
+     *
+     * @var array
+     */
+    protected $excludeTerms;
+
+    /**
+     * Get params
+     * @var array
+     */
+    protected $params = array();
+
+    /**
      * Contagged settings
+     *
      * @var array
      */
     protected $settings = array();
 
     /**
      * Term repository
+     *
      * @var TermRepository
      */
     protected $termRepository = null;
 
-
+    /**
+     * The terms
+     *
+     * @var \Speedbomb\Contagged\Domain\Model\Term[]
+     */
     protected $terms;
 
     /**
+     * The terms, sorted by length
+     *
+     * @var \Speedbomb\Contagged\Domain\Model\Term[]
+     */
+    protected $termsSorted;
+
+    /**
      * The TS config
+     *
      * @var array
      */
     protected $tsConfig = array();
@@ -104,7 +132,8 @@ class ParserService implements SingletonInterface {
             $this->initSettings();
 
             // Set StoragePid in the query settings object
-            $querySettings->setStoragePageIds(GeneralUtility::trimExplode(',', $this->tsConfig['persistence.']['storagePid']));
+            $querySettings->setStoragePageIds(GeneralUtility::trimExplode(',',
+                $this->tsConfig['persistence.']['storagePid']));
 
             // Set current language uid
             $querySettings->setLanguageUid($GLOBALS['TSFE']->sys_language_uid);
@@ -113,7 +142,82 @@ class ParserService implements SingletonInterface {
             $this->termRepository->setDefaultQuerySettings($querySettings);
         }
 
-        unset($querySettings, $configurationManager, $objectManager);
+        // get params
+        $get = GeneralUtility::_GET();
+        if(isset($get['tx_contagged_glossary'])) {
+            foreach($get['tx_contagged_glossary'] as $get_key => $get_value) {
+                $this->params[$get_key] = $get_value;
+            }
+        }
+
+        unset($querySettings, $configurationManager, $objectManager, $get);
+    }
+
+    /**
+     * Fetches the terms and sorts them
+     */
+    protected function fetchTerms() {
+        // fetch term from repository
+        $this->terms = $this->termRepository->findTerms();
+
+        // test if current page is the detail page
+        $this->excludeTerms = array();
+        if($GLOBALS['TSFE']->id == $this->settings['detailPage']) {
+            if($this->params['controller'] == 'Glossary' && $this->params['action'] == 'detail') {
+                /** @var \Speedbomb\Contagged\Domain\Model\Term $current_term */
+                $current_term = $this->termRepository->findByUid(intval($this->params['term']));
+                if($current_term != null) {
+                    // save all term variations of the current term
+                    $this->excludeTerms = array_merge(array($current_term->getTermMain()), GeneralUtility::trimExplode("\n", $current_term->getTermAlt(), true));
+                }
+                unset($current_term);
+            }
+        }
+
+        // sort terms
+        $this->termsSorted = array();
+return;
+        // @todo refactor for use as array of objects
+        foreach($this->terms as $term_key => $term_array) {
+            if($this->settings['autoExcludeTerms'] && in_array($term_array['term_main'], $this->excludeTerms)) {
+                continue;
+            }
+
+            $this->termsSorted[] = array('term' => $term_array['term_main'], 'key' => $term_key);
+            if(is_array($term_array['term_alt'])) {
+                foreach($term_array['term_alt'] as $term) {
+                    if($this->settings['autoExcludeTerms'] && in_array($term, $this->excludeTerms)) {
+                        continue;
+                    }
+                    $this->termsSorted[] = array('term' => $term, 'key' => $term_key);
+                }
+            }
+        }
+
+        // sort the array descending by length of the value, so the longest term will match
+        usort($this->termsSorted, array($this, 'sortTermsByDescendingLength'));
+    }
+
+    /**
+     * Some content tagged by configured tags could be prevented from beeing parsed.
+     * This function collects all the tags which should be considered.
+     *
+     * @return string Comma separated list of tags
+     */
+    protected function getTagsToOmitt() {
+        $tagArray = $this->settings['excludeTags'];
+
+        // if configured: add tags used by the term definitions
+        // @todo span with class add 'span'... test it
+        if($this->settings['autoExcludeTags'] > 0) {
+            foreach($this->tsConfig['types.'] as $key => $type) {
+                if(!empty($type['tag']) && !in_array($type['tag'], $tagArray)) {
+                    $tagArray[] = $type['tag'];
+                }
+            }
+        }
+
+        return implode(',', $tagArray);
     }
 
     /**
@@ -124,7 +228,7 @@ class ParserService implements SingletonInterface {
         $this->settings = GeneralUtility::removeDotsFromTS($this->tsConfig['settings.']);
 
         // type conversion: boolean
-        foreach(array('disableParser') as $setting) {
+        foreach(array('disableParser', 'autoExcludeTags', 'useSearchTags', 'parseSingleView') as $setting) {
             $this->settings[$setting] = $this->settings[$setting] == true;
         }
 
@@ -134,62 +238,15 @@ class ParserService implements SingletonInterface {
         }
 
         // type conversion: list to array
-        foreach(array('includeRootPages', 'excludeRootPages', 'includePages', 'excludePages') as $setting) {
-            $this->settings[$setting] =  GeneralUtility::trimExplode(',', $this->settings[$setting], true);
+        foreach(array(
+            'includeRootPages',
+            'excludeRootPages',
+            'includePages',
+            'excludePages',
+            'excludeTags'
+        ) as $setting) {
+            $this->settings[$setting] = GeneralUtility::trimExplode(',', $this->settings[$setting], true);
         }
-    }
-
-    /**
-     * Main function called by hook 'contentPostProc-all'
-     *
-     * @param \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController[] $pobjs Controller references
-     * @param \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController $controller Controller
-     * @return void
-     */
-    public function pageParser($pobjs, &$controller) {
-
-        // Abort parser if no settings available or
-        if(!is_array($this->settings) || count($this->settings) == 0 ||
-            // parser is disabled
-            $this->settings['disableParser'] == true ||
-            // Pagetype not 0
-            $GLOBALS['TSFE']->type !== 0 ||
-            // current page is the glossary detailpage
-            $GLOBALS['TSFE']->id == $this->settings['detailPage'] ||
-            // current page is the glossary listpage
-            $GLOBALS['TSFE']->id == $this->settings['listPage'] ||
-            // no config is given
-            count($this->tsConfig) == 0 ||
-            // page skip test
-            $this->isPageToSkip()
-        ) {
-            return;
-        }
-
-        // get the terms
-        $this->terms = $this->termRepository->findTerms();
-
-        // get the content
-        $content = $controller->content;
-        $htmlParser = GeneralUtility::makeInstance('TYPO3\CMS\Core\Html\HtmlParser');
-        $splittedContent = $htmlParser->splitIntoBlock($this->getTagsToOmitt(), $content);
-
-        foreach ((array)$splittedContent as $intKey => $HTMLvalue) {
-            if (!($intKey % 2)) {
-                $positionsArray = array();
-                foreach ($sortedTerms as $termAndKey) {
-                    if (empty($termAndKey['term'])) {
-                        continue;
-                    }
-                    $this->getPositions($splittedContent[$intKey], $positionsArray, $termAndKey['term'], $termAndKey['key']);
-                }
-                ksort($positionsArray);
-                $splittedContent[$intKey] = $this->doReplace($splittedContent[$intKey], $positionsArray);
-            }
-        }
-        $parsedContent = implode('', $splittedContent);
-
-        $controller->content = $parsedContent;
     }
 
     /**
@@ -249,5 +306,91 @@ class ParserService implements SingletonInterface {
         */
 
         return $skip;
+    }
+
+    /**
+     * Main function called by hook 'contentPostProc-all'
+     *
+     * @param \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController[] $pobjs Controller references
+     * @param \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController $controller Controller
+     * @return void
+     */
+    public function pageParser($pobjs, &$controller) {
+
+        // Abort parser if no settings available or
+        if(!is_array($this->settings) || count($this->settings) == 0 ||
+            // parser is disabled
+            $this->settings['disableParser'] == true ||
+            // Pagetype not 0
+            $GLOBALS['TSFE']->type !== 0 ||
+            // current page is the glossary detailpage and single page parsing is not allowed
+            ($GLOBALS['TSFE']->id == $this->settings['detailPage'] && $this->settings['parseSingleView'] != true) ||
+            // current page is the glossary listpage
+            $GLOBALS['TSFE']->id == $this->settings['listPage'] ||
+            // no config is given
+            count($this->tsConfig) == 0 ||
+            // page skip test
+            $this->isPageToSkip()
+        ) {
+            return;
+        }
+
+        // fetch the terms
+        $this->fetchTerms();
+
+        // get the content
+        #$content = $controller->content; // complete html
+        $content = $GLOBALS['TSFE']->getPageRenderer()->getBodyContent(); // body content
+
+        // if set, the contents could be limited to content within the TYPO3SEARCH tags
+        if($this->settings['useSearchTags']) {
+            /** @var \TYPO3\CMS\IndexedSearch\Indexer $indexer */
+            $indexer = GeneralUtility::makeInstance('TYPO3\CMS\IndexedSearch\Indexer');
+            $indexer->typoSearchTags($content);
+            unset($indexer);
+        }
+
+        $htmlParser = GeneralUtility::makeInstance('TYPO3\CMS\Core\Html\HtmlParser');
+        $splittedContent = $htmlParser->splitIntoBlock($this->getTagsToOmitt(), $content);
+
+        return;
+
+        foreach((array) $splittedContent as $intKey => $HTMLvalue) {
+            if(!($intKey % 2)) {
+                $positionsArray = array();
+                foreach($sortedTerms as $termAndKey) {
+                    if(empty($termAndKey['term'])) {
+                        continue;
+                    }
+                    $this->getPositions($splittedContent[$intKey], $positionsArray, $termAndKey['term'],
+                        $termAndKey['key']);
+                }
+                ksort($positionsArray);
+                $splittedContent[$intKey] = $this->doReplace($splittedContent[$intKey], $positionsArray);
+            }
+        }
+        $parsedContent = implode('', $splittedContent);
+
+        $controller->content = $parsedContent;
+    }
+
+    /**
+     * Utility method to sort array items according to the (string) length of their 'term' item.
+     *
+     * Note: the sorting is descending
+     *
+     * @param array $a Array with key 'term' containing a term
+     * @param array $b Array with key 'term' containing a term
+     * @return integer +1 if term from a is shorter than b, -1 for the contrary, 0 in case of equality
+     */
+    public static function sortTermsByDescendingLength($a, $b) {
+        // Calculate length correctly by relying on \TYPO3\CMS\Core\Charset\CharsetConverter
+        $aTermLength = $GLOBALS['TSFE']->csConvObj->strlen($GLOBALS['TSFE']->renderCharset, $a['term']);
+        $bTermLength = $GLOBALS['TSFE']->csConvObj->strlen($GLOBALS['TSFE']->renderCharset, $b['term']);
+        if($aTermLength == $bTermLength) {
+            return 0;
+        } else {
+            return ($aTermLength < $bTermLength) ? +1 : -1;
+        }
     }
 }
